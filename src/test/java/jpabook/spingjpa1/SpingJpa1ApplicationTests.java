1db5db68a272c63ea873774ade9f0395e5b4fa13
keeptrynjoy <sungmin4218@gmail.com>
2023-03-06 05:05:40 +0900

    기본 세팅

---
+++ src/test/java/jpabook/spingjpa1/SpingJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.spingjpa1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingJpa1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
