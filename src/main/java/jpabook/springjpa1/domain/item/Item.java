0e61c12f0356b23ad06c7b10370b50efd514addb
keeptrynjoy <sungmin4218@gmail.com>
2023-03-07 17:37:45 +0900

    item 로직 추가

--- src/main/java/jpabook/springjpa1/domain/item/Item.java
+++ src/main/java/jpabook/springjpa1/domain/item/Item.java
@@ -1,5 +1,6 @@
 package jpabook.springjpa1.domain.item;
 
+import jpabook.springjpa1.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,29 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    //== 비즈니스 로직 ==//
+
+    /**
+    * stock 증가
+    * */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+    * stock 감소
+    * */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+
+        this.stockQuantity = restStock;
+    }
+
+
 }
