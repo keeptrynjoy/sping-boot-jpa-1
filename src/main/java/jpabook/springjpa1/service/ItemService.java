2addb30c3dbee5f558403ae4de8e7085a1a920f8
keeptrynjoy <sungmin4218@gmail.com>
2023-03-08 16:00:06 +0900

    상품 등록/조회 로직

--- src/main/java/jpabook/springjpa1/service/ItemService.java
+++ src/main/java/jpabook/springjpa1/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.springjpa1.service;
 
+import jpabook.springjpa1.domain.item.Book;
 import jpabook.springjpa1.domain.item.Item;
 import jpabook.springjpa1.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,15 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item){
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity ){
+        // 영속상태
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems(){
